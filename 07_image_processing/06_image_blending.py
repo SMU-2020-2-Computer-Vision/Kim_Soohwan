fa2b044411e71ec472ab7b0d47492083335300cc
kim-soo-hwan <kimsoohwan@sunmoon.ac.kr>
2020-10-06 17:34:46 +0900

    Fixed some typo in the code

--- 07_image_processing/06_image_blending.py
+++ 07_image_processing/06_image_blending.py
@@ -13,11 +13,11 @@ img_park = cv2.imread('park.png')
 img_tom = cv2.imread('tom.png')
 
 # Blend those two images
-cv2.imshow('Image Blening', cv2.hconcat([img_park, img_park, img_tom]))
+cv2.imshow('Image Blending', cv2.hconcat([img_park, img_park, img_tom]))
 cv2.waitKey(0)
 for i in np.linspace(0, 1, 100):
     dst = cv2.addWeighted(img_park, 1-i, img_tom, i, 0)
-    cv2.imshow('Image Blening', cv2.hconcat([img_park, dst, img_tom]))
+    cv2.imshow('Image Blending', cv2.hconcat([img_park, dst, img_tom]))
     cv2.waitKey(30)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
